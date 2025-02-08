447532cc79b88763e8deeafc4cbd658549239f04
YegorPanin <66701715+YegorPanin@users.noreply.github.com>
2025-02-08 13:32:10 +0300

    Add files via upload

--- Clicker/main.py
+++ Clicker/main.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+import pygame
+import sys
+
+#Инициализация необходимых объектов
+scr = pygame.display.set_mode((800,600))
+
+#Игровой цикл
+while True:
+
+    scr.fill((255, 255, 255))
+    pygame.display.update()
+
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
